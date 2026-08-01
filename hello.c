61f67e0ba6894359942c1b66cd81395f949c7d06
nibir-k <nibirkalita75@gmail.com>
2026-08-02 01:57:45 +0530

    Updated hello.c program

--- hello.c
+++ hello.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
 
-int main(void) {
-    int age;
-
-    // Prompt user for input with a clean format
-    printf("Enter your age: ");
-    
-    // Read input and check if valid integer was entered
-    if (scanf("%d", &age) == 1) {
-        printf("Age is: %d\n", age);
-    } else {
-        printf("Invalid input! Please enter a valid number.\n");
-    }
-
+int main() {
+    int age = 22;
+    printf("age is %d \n",age);
     return 0;
 }
